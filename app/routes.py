a182382578582d75a3584f83d138a15189d5a5fe
Corinne <corinnefwhite@gmail.com>
2020-10-07 10:46:13 +0200

    Basic routing file for hello world

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+
+# Python function decorators.
+# in this case they register the function as a callback for a certain event, eg. hitting the URL "/" or "/index"
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hellos World!"
+
