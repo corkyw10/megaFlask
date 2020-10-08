2a4a3f3cf7dd9a1d451d1f517924e0872d8792ad
Corinne <corinnefwhite@gmail.com>
2020-10-08 10:40:39 +0200

    Added login page route

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 # Python function decorators.
 # in this case they register the function as a callback for a certain event, eg. hitting the URL "/" or "/index"
@@ -18,4 +19,9 @@ def index():
         }
     ]
     return render_template('index.html', title="Home", user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return(render_template('login.html', title='Sign In', form=form))
 
