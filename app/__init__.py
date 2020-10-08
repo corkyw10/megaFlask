f8c9884d601b563e4cb0314c4a174513179dbdb1
Corinne <corinnefwhite@gmail.com>
2020-10-08 17:31:48 +0200

    Initialise SQLAlchemy  and Migration modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 # Create an instance of Flask
 # __name__ is a predefined Python variable that refers to the name of the module in which it is used
 # Flask uses the name of the module as the starting point to load any associated resources
 app = Flask(__name__)
 app.config.from_object(Config)
-
-from app import routes
+# most flask extensions are initialised as so:
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+# model module defines the structure of the database
+from app import routes, models
 
