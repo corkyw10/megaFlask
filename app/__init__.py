67dc3c0638982377fc212314b04ec4d5714ba800
Corinne <corinnefwhite@gmail.com>
2020-10-08 10:12:42 +0200

    import config to init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from config import Config
 
 # Create an instance of Flask
 # __name__ is a predefined Python variable that refers to the name of the module in which it is used
 # Flask uses the name of the module as the starting point to load any associated resources
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
 
