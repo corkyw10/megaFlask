4efaef83e25a8ae05fbdfd6439751db5d0dbe5b0
Corinne <corinnefwhite@gmail.com>
2020-10-09 16:54:59 +0200

    Add redirect to previous page that checks for relative path if user is forced to log in

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
 from app.models import User
+from werkzeug.urls import url_parse
 
 # Python function decorators.
 # in this case they register the function as a callback for a certain event, eg. hitting the URL "/" or "/index"
@@ -39,7 +40,10 @@ def login():
             return redirect(url_for('login'))
         # Log user in and remember based on user preference, redirect
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return(render_template('login.html', title='Sign In', form=form))
 
 @app.route('/logout')
