b408960e6d92eb06abeaf79028e48c85e5c48706
Corinne <corinnefwhite@gmail.com>
2020-10-09 15:45:04 +0200

    Added login logic to check user and password and redirect logged in user to index

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 # Python function decorators.
 # in this case they register the function as a callback for a certain event, eg. hitting the URL "/" or "/index"
@@ -22,10 +24,20 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # If the current user is already authenticate redirect to index page
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    # Initialise the Login form
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        # search and return result for username in user table
+        user = User.query.filter_by(username=form.username.data).first()
+        # Warning will flash if username doesn't exist or password is incorrect
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        # Log user in and remember based on user preference, redirect
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return(render_template('login.html', title='Sign In', form=form))
 
