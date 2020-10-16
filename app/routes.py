c6048d778621e62c846d294f46f2540d276fd4ab
Corinne <corinnewhite@live.co.uk>
2020-10-16 14:11:32 +0200

    Added routes for following and unfollowing. Empty field form added to user profile page for follow/unfollow button

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from app.models import User
 from werkzeug.urls import url_parse
 
@@ -80,7 +80,8 @@ def user(username):
         {'author': user, 'body': 'Mighty Morphin!'},
         {'author': user, 'body': 'Power Rangers!'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 @app.route('/edit_profile', methods=["GET", "POST"])
 @login_required
@@ -96,3 +97,41 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title="Edit Profile", form=form)
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('Ranger {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('Rita Repulsa has tricked you! You can\'t follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('Ranger {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('Lord Zedd has tricked you! You can\'t follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        flash('You are following {}!'.format(username))
+        db.session.commit()
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
